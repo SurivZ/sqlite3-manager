98bf6fd01e1d95ede66557b34c3c7099f2116ace
David Serrano <franklinserrano23@gmail.com>
2024-10-22 01:54:29 -0500

    Corrección del setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Este paquete proporciona una serie de funcionalidades para gestionar bases de datos SQLite3 de manera sencilla y estandarizada.',
     long_description=open('readme.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/SurivZ/Py-SQLite-DB-Connection',
+    url='https://github.com/SurivZ/sqlite3-manager',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
